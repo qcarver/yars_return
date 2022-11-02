f6f7131c9c471ce498531c9da65ae14704d0d3c8
Quinn <qcarver@gmail.com>
2022-11-02 17:52:36 -0400

    Update yars_return.asm
    Added basic joystick routine

--- yars_return.asm
+++ yars_return.asm
@@ -1,27 +1,52 @@
 	processor 6502
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Include required files with VCS register memory mapping and macros
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	include "vcs.h"
 	include "macro.h"
-	SEG.U VARS
-	ORG $80
 
-YarXPos	byte	; Positions
-YarYPos byte	;
-SwirlXPos byte  ;
-SwirlYPos byte  ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Declare the variables starting from memory address $80
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	seg.u VARS
+	org $80
 
+YarXPos		byte	; Positions
+YarYPos 	byte	;
+QuotileXPos 	byte    ;
+QuotileYPos 	byte	;
+YarSpritePtr    word	; Pointers
+YarColorPtr     word	;
+QuotileSpritePtr	word	;
+QuotileColorPtr	word	;
 
-	SEG CODE
-	ORG $F000
-Start:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define constants
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+YAR_HEIGHT = 	9	; Height of Yar Sprite
+QUOTILE_HEIGHT= 9	; Height of Quotile
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start our ROM code at memory address $F000
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        seg Code	
+	org $F000
+
+Reset:
 	CLEAN_START
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initialize RAM variables and TIA registers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	lda #10
-	sta YarYPos
 	lda #60
+	sta YarYPos
+	lda #80
 	sta YarXPos
+	lda #52
+	sta QuotileXPos 
+	lda #8
+	sta QuotileYPos
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Initialize the pointers to LUTs 
@@ -36,27 +61,39 @@ Start:
 	lda #>YarColor
 	sta YarColorPtr+1	;hi-byte for address of Yar sprite	
 
-	lda #<SwirlSprite
-	sta SwirlSpritePtr	;lo-byte for address of Swirl sprite
-	lda #>SwirlSprite
-	sta SwirlSpritePtr+1	;hi-byte for address of Swirl sprite	
+	lda #<QuotileSprite
+	sta QuotileSpritePtr	;lo-byte for address of Quotile sprite
+	lda #>QuotileSprite
+	sta QuotileSpritePtr+1	;hi-byte for address of Quotile sprite	
 
-	lda #<SwirlColor
-	sta SwirlColorPtr	;lo-byte for address of Swirl sprite
-	lda #>SwirlColor
-	sta SwirlColorPtr+1	;hi-byte for address of Swirl sprite	
+	lda #<QuotileColor
+	sta QuotileColorPtr	;lo-byte for address of Quotile sprite
+	lda #>QuotileColor
+	sta QuotileColorPtr+1	;hi-byte for address of Quotile sprite	
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Repeating playfield is default with clean start
-;	lda #1
-;	sta CTRLPF	; Mirrored playfield
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start the main display loop and frame rendering
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+StartFrame:
 
-StartFrame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Calculations and tasks performed in the pre-VBlank 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	lda YarXPos		; this will be the A parameter for the subr
+	ldy #0			; this will be the object type paramter 
+	jsr SetObjectXPos	; set Yar horizontal position, call subroutine
+
+	lda QuotileXPos		; same as above, but Quotile instead of Yar
+	ldy #1
+	jsr SetObjectXPos	; set player1 horizontal position
+
+	sta WSYNC
+	sta HMOVE		; apply the offsets
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; VSYNCH and VBLANK
+;; Display VSYNC and VBLANK
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-	lda INTIM
+	lda #2
 	sta VBLANK
 	sta VSYNC
 	REPEAT 3
@@ -70,18 +107,18 @@ StartFrame
 	sta VBLANK
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Visible Frame
+;; Visible Frame: display 192 scanlines
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-PlayfieldLoop
+GameVisibleLine
+	ldy #$A0		; make sky...	
+	sty COLUBK		; bg color blue 
  
-	ldx #192				; use x as playfield line counter
-
+	ldx #96		; row cnt:192:1,96:2line kernel 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Visible Row
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-.GameLineLoop
-	ldy #$A0		; make sky...	
-	sty COLUBK		; 	bg color blue
+.GameLineLoop:
+        ;;BEGIN: Draw Playfield______________
 	lda PFColors,x		; get color...
 	sta COLUPF		; 	for row
 	lda PF0DataA,x		; get bit..
@@ -91,13 +128,44 @@ PlayfieldLoop
 	lda PF2DataA,x		; 	playfield
 	sta PF2			; 	vars
 	sta WSYNC
+	;;END: Draw Playfield________________
+
+.AreWeInsideYarSprite:
+	txa		; transfer linePos to A
+	sec		; sec (before subtract)
+	sbc YarYPos	; SpriteY - linepos
+	cmp YAR_HEIGHT	; w/in height of sprite?
+	bcc .DrawYarSprite
+	lda #0		; else: set  empty sprite line
+
+.DrawYarSprite
+	tay		; hint: Y only indirect register
+	lda (YarSpritePtr),y	;ld Sprite at offset
+	;;sta WSYNC	; wait for scanline
+	sta GRP0	; set graphics for Yar
+	lda (YarColorPtr),y	;load color
+	sta COLUP0	; set color of Yar
+
+.AreWeInsideQuotileSprite:
+	txa		; transfer linePos to A
+	sec		; sec (before subtract)
+	sbc QuotileYPos	; SpriteY - linepos
+	cmp QUOTILE_HEIGHT	; w/in height of sprite?
+	bcc .DrawQuotileSprite
+	lda #0		; else: set  empty sprite line
+
+.DrawQuotileSprite
+	tay		; hint: Y only indirect register
+	lda #5		; b101 stretches sprite Horizon
+	sta NUSIZ1
+	lda (QuotileSpritePtr),y	;ld Sprite at offset
+	sta WSYNC	; wait for scanline
+	sta GRP1	; set graphics for Quotile
+	lda (QuotileColorPtr),y	;load color
+	sta COLUP1	; set color of Quotile
+
 	dex			; x--
 	bne .GameLineLoop
-	
-	;;something glitchy at top, make it blend in
-	ldy #$A0		; make sky...	
-	sty COLUBK		; bg color blue 
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Display Overscan
@@ -105,22 +173,70 @@ PlayfieldLoop
 	lda #2
 	sta VBLANK				; turn on VBLANK again
 	REPEAT 30
-		sta WSYNC			; display 30 recommended lines of VBlank Overscan
+		sta WSYNC			; 30 blank lines of overscan 
 	REPEND
 	lda #0
 	sta VBLANK				; turn off VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Process joystick input for player0 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+CheckP0Up:
+	lda #%00010000	; player0 joystick up
+	bit SWCHA	
+	bne CheckP0Down	; If bit patter doesn't bypass UP
+        inc YarYPos	
+CheckP0Down:
+	lda #%00100000   ; player0 joystick down
+	bit SWCHA
+	bne CheckP0Left  ; If bit pattern doesn't match, bypass Down Block
+        dec YarYPos	
+CheckP0Left:
+	lda #%01000000   ; player0 joystick down
+	bit SWCHA
+	bne CheckP0Right  ; If bit pattern doesn't match, bypass Left Block
+        dec YarXPos	
+CheckP0Right:
+	lda #%10000000   ; player0 joystick down
+	bit SWCHA
+	bne EndInputCheck  ; If bit pattern doesn't match, bypass Right Block
+        inc YarXPos	
+EndInputCheck
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Loop back to start a brand new frame
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	jmp StartFrame			; continue to display the next frame
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Subroutine to handle object horizontal position w/ fine offset 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; A is the target x-coordinate position in pixels of our target
+;; Y is the object type (0:player0, 1:player1, 2:missile0, 3:missle1, 4:ball) 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+SetObjectXPos subroutine
+	sta WSYNC		; start a fresh new scanline
+	sec			; set the carry-flag before subtracting
+.Div15Loop			; divide is just continuos subtraction
+	sbc #15			; subtract 15 from A in accumulator
+	bcs .Div15Loop		; loop until carry-flag is clear
+	eor #7			; xor signs corse remainder from -8 to 7
+	asl
+	asl
+	asl
+	asl		; move four lowest bits to hi (littleendian)
+	sta HMP0,Y		; store the fine offset to the correct HMxx
+	sta RESP0,Y		; fix object position in 15 step increment
+	rts			; return	
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Player Graphic and Color
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-YarSprites:
+YarSprite:
 Yar_0
 	.byte $00 ; |		|
 	.byte $0E ; |	XXX |
@@ -131,6 +247,7 @@ Yar_0
 	.byte $8D ; |X	XX X|
 	.byte $08 ; |	X	|
 	.byte $0E ; |	XXX |
+;;YAR_HEIGHT = . - Yar_0		; macro, dot means curr location
 Yar_1  
 	.byte $00 ; |		|
 	.byte $40 ; | X	  |
@@ -143,8 +260,8 @@ Yar_1
 	.byte $40 ; | X	  |
 
 
-SwirlSprites
-Swirl_0	
+QuotileSprite
+Quotile_0	
 	.byte $00 ; |........|
 	.byte $60 ; |.XX.....|
 	.byte $11 ; |...X...X|
@@ -154,7 +271,8 @@ Swirl_0
 	.byte $90 ; |X..X....|
 	.byte $88 ; |X...X...|
 	.byte $06 ; |.....XX.|
-Swirl_1	
+;;QUOTILE_HEIGHT = . - Quotile_0 alt way to specify height
+Quotile_1	
 	.byte $00 ; |........|
 	.byte $04 ; |.....X..|
 	.byte $62 ; |.XX...X.|
@@ -164,7 +282,7 @@ Swirl_1
 	.byte $49 ; |.X..X..X|
 	.byte $46 ; |.X...XX.|
 	.byte $20 ; |..X.....|
-Swirl_2	
+Quotile_2	
 	.byte $00 ; |........|
 	.byte $18 ; |...XX...|
 	.byte $06 ; |.....XX.|
@@ -186,7 +304,7 @@ YarColor:
 	byte #$0E	
 	byte #$0E
 	byte #$0E
-SwirlColor:
+QuotileColor:
 	byte #$00
 	byte #$46
 	byte #$46
@@ -986,6 +1104,5 @@ PFColors
 	ECHO ([$FFFC-.]d), "bytes free"
 
 	org $fffc
-	.word Start
-	.word Start
-
+	.word Reset
+	.word Reset
